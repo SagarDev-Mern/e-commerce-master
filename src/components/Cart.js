1d3567f88db91a0c764a526abe9133f257f927eb
Akshay Pawashe <akshay.pawashe@rubixds.com>
2024-08-12 00:12:01 +0530

    card changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Cart = () => {
+
+  const productList = useSelector(state => state.cartData)
+  console.log(productList)
+
   return (
     <div>
-        <h1>This is cart</h1>
+      <h1>Cart</h1>
+      <div className='table'>
+      {productList.map((product) =>
+        <div className='table-data' key={product.id}>
+          <img style={{height: '200px'}} src={product.photo} alt="" />
+          <div className='table-data-details'>
+            <h5>Product Name : {product.name}</h5>
+            <p>Price : {product.price}</p>
+            <p>Color : {product.color}</p>
+            <p>Type : {product.type}</p>
+          </div>
+        </div>
+      )}
+      </div>
     </div>
   )
 }
